ecd0d16184eb2d4e9bd24a7d816aac5cf0dddbc3
afzal mahmud <afzzalmahmud@gmail.com>
2021-08-21 16:26:41 +0600

    calculate total cost and update on click

---
+++ js/app.js
@@ -0,0 +1,85 @@
+/* giveInnerText return the inner text of prise as number */
+function giveInnerText(innerPrice) {
+   const getField = document.getElementById(innerPrice)
+   const getInnerNumber = parseInt(getField.innerText);
+   return getInnerNumber;
+}
+/* take all price and calculate with updateTotal function */
+    function updateTotal() {
+        // const parsePrice = parseInt(price);
+       const defaultM1Price = giveInnerText('best-price-cost');
+       const unifiedMemory= giveInnerText('ram-memory-cost');
+       const ssdStorage= giveInnerText('storage-memory-cost');
+       const deliveryCost= giveInnerText('delivery-cost');
+       const totalCost = defaultM1Price + unifiedMemory + ssdStorage +deliveryCost;
+
+       /* update total cost and discount total cost */
+       const updateTotalCost = document.getElementById('total-cost');
+       updateTotalCost.innerText = totalCost;
+
+       const updateDiscountTotal = document.getElementById('promo-discount-total');
+       updateDiscountTotal.innerText = totalCost;
+    }
+/* 
+set all price with addPrice function it take id 
+and the price basced on which btn you clicked
+*/
+function addPrice(id,price) {
+    const getPriceId = document.getElementById(id);
+    getPriceId.innerText = price;
+
+    updateTotal();
+}
+
+/* button for unified memory */
+const eightUnifiedBtn = document.getElementById('ram-memory-default');
+eightUnifiedBtn.addEventListener('click',function(){
+    
+    addPrice('ram-memory-cost',0)
+})
+
+const sixteenUnifiedBtn = document.getElementById('ram-memory-price');
+sixteenUnifiedBtn.addEventListener('click',function(){
+    
+    const setPrice = addPrice('ram-memory-cost',180)
+
+    
+})
+/* button for ssd storage */
+const ssdBtn256 = document.getElementById('ssd-storage-default');
+ssdBtn256.addEventListener('click',function () {
+    
+    const setPrice = addPrice('storage-memory-cost',0)
+
+})
+
+const ssdBtn512 = document.getElementById('ssd-storage-price1');
+ssdBtn512.addEventListener('click',function () {
+    
+    addPrice('storage-memory-cost',100)
+
+})
+
+const ssdBtn1TB = document.getElementById('ssd-storage-price2');
+ssdBtn1TB.addEventListener('click',function () {
+    
+    addPrice('storage-memory-cost',180)
+
+})
+
+/* button for delivery */
+const deliveryFreeBtn = document.getElementById('delivery-cost-default');
+deliveryFreeBtn.addEventListener('click',function () {
+
+    addPrice('delivery-cost',0)
+})
+
+const deliveryCostBtn = document.getElementById('delivery-cost-needed');
+deliveryCostBtn.addEventListener('click',function () {
+
+    addPrice('delivery-cost',20)
+})
+/* discunt and promo code */
+const promoInput = document.getElementById('promo-input')
+const promoBtn = document.getElementById('promo-btn')
+const discountTotal = document.getElementById('promo-discount-total')
