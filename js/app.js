cae4a27b5485786bafd6230c298f95dfc65b4508
afzal mahmud <afzzalmahmud@gmail.com>
2021-08-21 18:00:42 +0600

    add comment about project overview and calculate discount

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+/* 
+    project overview :
+1 . giveInnerText function take html id of as parameter and
+    return this id innerText as number;
+
+2 . updateTotal function take price and calculate the total;
+
+3 . addPrice function take html id and price based on 
+    whic button you clicked and call updateTotal function and 
+    calculate again;
+*/
+
 /* giveInnerText return the inner text of prise as number */
 function giveInnerText(innerPrice) {
    const getField = document.getElementById(innerPrice)
@@ -6,7 +18,7 @@ function giveInnerText(innerPrice) {
 }
 /* take all price and calculate with updateTotal function */
     function updateTotal() {
-        // const parsePrice = parseInt(price);
+
        const defaultM1Price = giveInnerText('best-price-cost');
        const unifiedMemory= giveInnerText('ram-memory-cost');
        const ssdStorage= giveInnerText('storage-memory-cost');
@@ -41,33 +53,29 @@ eightUnifiedBtn.addEventListener('click',function(){
 const sixteenUnifiedBtn = document.getElementById('ram-memory-price');
 sixteenUnifiedBtn.addEventListener('click',function(){
     
-    const setPrice = addPrice('ram-memory-cost',180)
-
-    
+    addPrice('ram-memory-cost',180)  
 })
+
 /* button for ssd storage */
 const ssdBtn256 = document.getElementById('ssd-storage-default');
 ssdBtn256.addEventListener('click',function () {
     
-    const setPrice = addPrice('storage-memory-cost',0)
-
+    addPrice('storage-memory-cost',0)
 })
 
 const ssdBtn512 = document.getElementById('ssd-storage-price1');
 ssdBtn512.addEventListener('click',function () {
     
     addPrice('storage-memory-cost',100)
-
 })
 
 const ssdBtn1TB = document.getElementById('ssd-storage-price2');
 ssdBtn1TB.addEventListener('click',function () {
     
     addPrice('storage-memory-cost',180)
-
 })
 
-/* button for delivery */
+/* button for delivery cost */
 const deliveryFreeBtn = document.getElementById('delivery-cost-default');
 deliveryFreeBtn.addEventListener('click',function () {
 
@@ -79,7 +87,23 @@ deliveryCostBtn.addEventListener('click',function () {
 
     addPrice('delivery-cost',20)
 })
+
 /* discunt and promo code */
-const promoInput = document.getElementById('promo-input')
-const promoBtn = document.getElementById('promo-btn')
-const discountTotal = document.getElementById('promo-discount-total')
+
+const promoBtn = document.getElementById('promo-btn');
+
+
+promoBtn.addEventListener('click',function () {
+    const promoInput = document.getElementById('promo-input');
+
+    if(promoInput.value == 'stevekaku'){
+        const discountId = giveInnerText('promo-discount-total');
+        const discountPrice = (discountId*20)/100;
+        const discountAmount = discountId-discountPrice;
+        /* take inner text of discount Total on UI */
+        const discountForPromo = document.getElementById('promo-discount-total')
+        discountForPromo.innerText = discountAmount;
+
+        promoInput.value = "";
+    }
+})
